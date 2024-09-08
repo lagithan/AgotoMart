21cb543412dfb4e782031c4488c7ef417fc3c7dd
lagithan <109454694+lagithan@users.noreply.github.com>
2024-09-09 00:19:43 +0530

    Delete Frontend/Backend directory

--- Frontend/Backend/Middleware/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinaryV2 } from 'cloudinary';
-
-cloudinaryV2.config({
-  cloud_name: 'dgihkeczq', // Wrap in quotes
-  api_key: '544725424315599', // Wrap in quotes
-  api_secret: 'C9k6lv-FSyhDIH749aPLKnMHiJY' // Wrap in quotes
-});
-
-export { cloudinaryV2 as cloudinary };
